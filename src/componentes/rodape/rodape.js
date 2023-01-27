56fdc7012610f33a1e716b6589e3d0bbb15cb408
paulo200455 <pcesar.sj2004@gmail.com>
2023-01-27 09:38:29 -0300

    projetos mobile
    a pagina projetos foi adaptada para o modo mobile

--- src/componentes/rodape/rodape.js
+++ src/componentes/rodape/rodape.js
@@ -7,13 +7,13 @@ const Rodape = (props) =>{
 
     return(
         <div className={`rodapePrincipal ${props.classSecundaria}`}>
-            <a href="mailto:pcesar.sj2004@gmail.com"><CgMail size={45}/></a>
+            <a href="mailto:pcesar.sj2004@gmail.com"><CgMail/></a>
 
-            <a href="https://github.com/PauloCesarSJ"><BsGithub size={40}/></a>
+            <a href="https://github.com/PauloCesarSJ"><BsGithub/></a>
 
-            <a href="https://www.instagram.com/paulo.cesar.oi/"><BsInstagram size={40}/></a>
+            <a href="https://www.instagram.com/paulo.cesar.oi/"><BsInstagram/></a>
             
-            <a href="https://www.linkedin.com/in/paulo-cesar-4428b8260/"><BsLinkedin size={40}/></a>
+            <a href="https://www.linkedin.com/in/paulo-cesar-4428b8260/"><BsLinkedin/></a>
         </div>
     )
 }
